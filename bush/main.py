292fc6d4220be415fa730f32b211557bac815d4e
okamos <himinato.k@gmail.com>
2017-04-29 00:26:23 +0900

    Improve global options

--- bush/main.py
+++ bush/main.py
@@ -7,28 +7,24 @@ import botocore
 import color
 
 
-parser = OptionParser()
+usage = "%prog arg1 [options]"
+version = "%s %s" % ("%prog", __init__.__version__)
+parser = OptionParser(usage=usage, version=version)
 
 parser.add_option("-p", "--profile",
                   action="store", type="string", dest="profile",
                   help="Use a specific profile from your credential file")
+parser.add_option("-r", "--region",
+                  action="store", type="string", dest="region",
+                  help="Use a specific region")
 parser.add_option("--access_key_id",
                   action="store", type="string", dest="access_key",
                   help="Use a specific AWS_ACCESS_KEY_ID")
 parser.add_option("--secret_key",
                   action="store", type="string", dest="secret_key",
                   help="Use a specific AWS_SECRET_ACCESS_KEY")
-parser.add_option("-r", "--region",
-                  action="store", type="string", dest="region",
-                  help="Use a specific region")
-parser.add_option("-v", "--version",
-                  action="store_true", dest="version",
-                  help="Display version")
 
 (options, args) = parser.parse_args()
-
-if options.version:
-    print(__init__.__version__)
 
 # Bush looks for credentials this order.
 #   1. Environment variables
