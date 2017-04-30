6871788ff2faedc87f2ea63a09ce3d8bfa5a428d
okamos <himinato.k@gmail.com>
2017-04-30 23:52:36 +0900

    Add region name option

--- bush/aws/session.py
+++ bush/aws/session.py
@@ -14,15 +14,19 @@ def create_session(options):
     #   4. ~/.aws/credentials with default profile
     access_key = os.environ.get('AWS_ACCESS_KEY_ID') or options.access_key
     secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY') or options.secret_key
+    params = {}
+    if options.region:
+        params['region_name'] = options.region
+
     if access_key and secret_key:
-        s = session.Session(aws_access_key_id=access_key,
-                            aws_secret_access_key=secret_key)
-    elif options.profile:
-        try:
-            s = session.Session(profile_name=options.profile)
-        except botocore.exceptions.ProfileNotFound as error:
-            print(color.red(error))
-            sys.exit(2)
-    else:
-        s = session.Session(profile_name=options.profile)
-    return s
+        params['aws_access_key_id'] = access_key
+        params['aws_secret_access_key'] = secret_key
+
+    if options.profile:
+        params['profile_name'] = options.profile
+
+    try:
+        return session.Session(**params)
+    except botocore.exceptions.ProfileNotFound as error:
+        print(color.red(error))
+        sys.exit(2)
